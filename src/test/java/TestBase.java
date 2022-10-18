d6c9a9b90fd0bd6a75f739b25186a36016f5d44a
rbrynkus <renata.brynkus@gmail.com>
2022-10-18 09:13:53 +0200

    Corrected TestBase class.

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -8,23 +8,43 @@ import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 import org.junit.jupiter.api.BeforeAll;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class TestBase {
 
     private static ApplicationProperties applicationProperties;
+    private static Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     @BeforeAll
     static void beforeAll() {
         applicationProperties = ApplicationProperties.getInstance();
     }
 
+    public static Map<String, String> getHeadersOrParams(String type) {
+        Map<String, String> requestValues = new HashMap<>();
+        switch (type) {
+            case "headers" -> {
+                requestValues.put("name", System.getProperty("city"));
+                requestValues.put("country", System.getProperty("country"));
+            }
+            case "params" -> {
+                requestValues.put("appid", System.getProperty("appId"));
+                requestValues.put("id", System.getProperty("id"));
+            }
+            default -> logger.info("-----> You can add header or parameters <-----");
+        }
+        return requestValues;
+    }
+
     public static RequestSpecification getRequestSpec() {
         return new RequestSpecBuilder()
                 .setBaseUri(System.getProperty("baseUri"))
-                .addParam("appid", System.getProperty("appId"))
-                .addParam("id", System.getProperty("id"))
-                .addHeader("name", System.getProperty("city"))
-                .addHeader("country", System.getProperty("country"))
+                .addParams(getHeadersOrParams("params"))
+                .addHeaders(getHeadersOrParams("headers"))
                 .setContentType(ContentType.JSON)
                 .build()
                 .filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
